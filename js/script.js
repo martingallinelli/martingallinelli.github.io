8a3a4e46b3755f08ea4b91e95f88e21cdb0a9463
martingallinelli <martiin.edlp@gmail.com>
2021-03-19 17:03:18 -0300

    work experience + footer

--- js/script.js
+++ js/script.js
@@ -1,9 +1,15 @@
-// capturo el boton hamburguesa y el menu
-const boton = document.getElementById('boton');
-const menu = document.getElementById('menu');
+/* MENU */
+    // capturo el boton hamburguesa y el menu
+    const boton = document.getElementById('boton');
+    const menu = document.getElementById('menu');
 
-// click sobre boton hamburguesa
-boton.addEventListener('click', () => {
-    // toggle - agregar/quitar clase
-    menu.classList.toggle('hidden');
-});
+    // click sobre boton hamburguesa
+    boton.addEventListener('click', () => {
+        // toggle - agregar/quitar clase
+        menu.classList.toggle('hidden');
+    });
+
+/* AÑO SITIO WEB */
+    var año = new Date().getFullYear();
+    const mostrarAño = document.getElementById('año');
+    mostrarAño.innerHTML = "&copy; " + año + ". All Rights Reserved.";
